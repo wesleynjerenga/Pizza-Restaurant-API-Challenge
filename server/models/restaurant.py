62696c9b15d31b96c814f4b2c8314f36b2a7a867
Wesley <wesleynjerenga9@gmail.com>
2025-06-13 09:47:18 +0300

    Add Restaurant model with relationships and serialization methods

---
+++ server/models/restaurant.py
@@ -0,0 +1,26 @@
+from server import db
+
+class Restaurant(db.Model):
+    __tablename__ = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    address = db.Column(db.String, nullable=False)
+
+    # Relationships
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='restaurant', cascade='all, delete-orphan')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'address': self.address
+        }
+
+    def to_dict_with_pizzas(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'address': self.address,
+            'pizzas': [rp.pizza.to_dict() for rp in self.restaurant_pizzas]
+        } 
