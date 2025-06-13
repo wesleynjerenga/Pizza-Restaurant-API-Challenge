745f78d5d045ef3550f02587c39a44323d9b7f82
Wesley <wesleynjerenga9@gmail.com>
2025-06-13 09:47:20 +0300

    Add Pizza model with relationships and serialization method

---
+++ server/models/pizza.py
@@ -0,0 +1,18 @@
+from server import db
+
+class Pizza(db.Model):
+    __tablename__ = 'pizzas'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    ingredients = db.Column(db.String, nullable=False)
+
+    # Relationships
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='pizza')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'ingredients': self.ingredients
+        } 
